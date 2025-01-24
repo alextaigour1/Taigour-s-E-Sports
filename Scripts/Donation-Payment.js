d26840cd6f1bf777ed15d9e3f11e83787b28f91a
Taigour's E-Sports <alextaigour1@gmail.com>
2025-01-24 17:55:05 +0545

    Delete Scripts directory

--- Scripts/Donation-Payment.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const amountInput = document.getElementById('amount');
-    const totalAmountField = document.getElementById('total_amount');
-
-    amountInput.addEventListener('input', function () {
-        totalAmountField.value = amountInput.value;
-    });
-});
--- Scripts/Option-payment.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const amountSelect = document.getElementById('amount');
-    const totalAmountField = document.getElementById('total_amount');
-
-    amountSelect.addEventListener('change', function () {
-        totalAmountField.value = amountSelect.value;
-    });
-});
